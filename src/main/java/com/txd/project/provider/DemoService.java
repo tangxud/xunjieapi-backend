dd7481b63bcefda2bb78bdcd76bce16dae3748f2
tangxudong <tangxudong@shunyouedp.com>
2024-01-17 17:05:57 +0800

    引入dubbo、nacos

---
+++ src/main/java/com/txd/project/provider/DemoService.java
@@ -0,0 +1,19 @@
+package com.txd.project.provider;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * 示例服务
+ *
+ */
+public interface DemoService {
+
+    String sayHello(String name);
+
+    String sayHello2(String name);
+
+    default CompletableFuture<String> sayHelloAsync(String name) {
+        return CompletableFuture.completedFuture(sayHello(name));
+    }
+
+}
